22b9fe8615ba473ecf3dd9ea34c35ca10b4588b7
Ivan Krechetov <ikr@ikr.su>
2016-05-27 20:02:37 +0200

    Ex1 v1

--- ch9.js
+++ ch9.js
@@ -33,3 +33,65 @@ function print(x) {
 const cat = R.compose(R.map(print), readFile);
 
 console.dir(cat(__dirname + '/.babelrc').unsafePerformIO().unsafePerformIO());
+
+
+class Maybe {
+    constructor(x) {
+        this.__value = x;
+    }
+
+    isNothing() {
+        return (
+            this.__value === null ||
+            this.__value === undefined
+        );
+    }
+
+    map(f) {
+        return (
+            this.isNothing() ?
+            Maybe.of(null) :
+            Maybe.of(f(this.__value))
+        );
+    }
+
+    static of(x) {
+        return new Maybe(x);
+    }
+
+    join() {
+        return this.isNothing() ? Maybe.of(null) : this.__value;
+    }
+
+    chain(f) {
+        return this.map(f).join();
+    }
+}
+
+// Exercise 1
+// ==========
+// Use safeProp and map/join or chain to safely get the street name when given
+// a user.
+
+const safeProp = R.curry((x, o) => Maybe.of(o[x]));
+
+const user = {
+    id: 2,
+    name: 'albert',
+    address: {
+        street: {
+            number: 22,
+            name: 'Walnut St'
+        }
+    }
+};
+
+// safeStreetAddress :: User -> Maybe String
+const safeStreetAddress = R.compose(
+    R.chain(safeProp('name')),
+    R.chain(safeProp('street')),
+    safeProp('address')
+);
+
+console.dir(safeStreetAddress(user));
+console.dir(safeStreetAddress({}));
